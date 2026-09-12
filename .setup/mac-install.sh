02eee1be19ac581866bfeb9ea9b864d53bc4c216
Josh <josh@users-Mac-mini.local>
2026-09-12 07:27:09 -0700

    tweaks to script

--- .setup/mac-install.sh
+++ .setup/mac-install.sh
@@ -1,8 +1,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install stow kitty neovim
+brew install stow kitty neovim omniwm
 brew install rg fzf
 
-
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 defaults write -g InitialKeyRepeat -float 10.0 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -float 1.0 # normal minimum is 2 (30 ms)
+cd ~/dotfiles
+stow .
